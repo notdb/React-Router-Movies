d3660eb3a93dcb791775764b1e0fbbc99bb0c36d
Doug <me@douglasbowen.com>
2019-05-06 19:02:16 -0400

    adds the ability to click the movie name and be taken to a page with that movie on it

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default class App extends Component {
   constructor() {
@@ -22,7 +22,11 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <div>
+          Replace this Div with your Routes
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/:id" component={Movie} />
+        </div>
       </div>
     );
   }
